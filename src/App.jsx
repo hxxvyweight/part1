ce3370e41bc9ee632a95c0121cb27f77d8d51303
hxxvyweight <linuxtest923@gmail.com>
2026-08-02 11:07:01 +1000

    feat(part1): complete exercise 1.2 refractoring

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ const Header = (props) => {
 
 const Part = ({name, exercises }) => {
   return (
-    <p> {name} {exercise}</p>
+    <p> {name} {exercises}</p>
   )
 }
 
@@ -17,9 +17,9 @@ const Content = (props) => {
 
   return (
     <>
-    <p>{props.part1} {props.exercises1}</p>
-    <p>{props.part2} {props.exercises2}</p>
-    <p>{props.part3} {props.exercises3}</p>
+    <Part name={props.part1} exercises={props.exercises1}/>
+    <Part name={props.part2} exercises={props.exercises2}/>
+    <Part name={props.part3} exercises={props.exercises3}/>
     </>
   )
 
